6aa850349b8217f4185a7950e114656a1e4789ee
Sakshi <shankarsakshi03@gmail.com>
2021-12-24 22:40:05 +0530

    Check

---
+++ src/components/footer/index.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            Tried making with care (´◡`), by : {` `}
+            <a className="nav-link" href="https://github.com/sakshi037">
+                Sakshi_Abhishek
+            </a>
+        </div>
+    )
+}
+
+export default Footer
