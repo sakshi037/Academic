8dd666362855782dda376b1ad18e313028ea70b3
Sakshi <shankarsakshi03@gmail.com>
2021-12-24 23:21:14 +0530

    Check

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,8 +2,8 @@ const Footer = () => {
     return (
         <div className="footer">
             Tried making with care (´◡`), by : {` `}
-            <a className="nav-link" href="https://github.com/sakshi037">
-                Sakshi_Abhishek
+            <a className="nav-link" href="https://github.com/jatinsajwan3841">
+                Jatin
             </a>
         </div>
     )
