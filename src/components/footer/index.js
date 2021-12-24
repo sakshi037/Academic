937cebbe2718729164181ca82c8b7ed45b8110a4
Sakshi <shankarsakshi03@gmail.com>
2021-12-25 00:51:18 +0530

    Modified

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,6 +5,10 @@ const Footer = () => {
             <a className="nav-link" href="https://github.com/jatinsajwan3841">
                 Jatin
             </a>
+            <br></br>
+            <a className="nav-link" href="https://github.com/sakshi037">
+                Contribution by Sakshi Shankar
+            </a>
         </div>
     )
 }
