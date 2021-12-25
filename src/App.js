89d34f290319795c5a0a87a3ca09bc20fb1474e3
Sakshi <shankarsakshi03@gmail.com>
2021-12-25 15:41:48 +0530

    Bg

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = () => {
                     />
                 </Router>
             </div>
-            <ParticlesBg type="square" num="20" color="#a6a4ad" />
+            <ParticlesBg type="square" num="20" />
             <Footer />
         </div>
     )
